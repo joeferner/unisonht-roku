a40ee7d618dee3a8db6a488f27309a56361787d3
Joe Ferner <joe@fernsroth.com>
2020-09-24 08:32:04 -0400

    add build to pre-commit

--- src/test.ts
+++ src/test.ts
@@ -6,21 +6,21 @@ const unisonht = new UnisonHT({});
 unisonht.use(new WebApi({ port }));
 
 unisonht.use(
-  new Roku('roku', {
-    url: 'http://192.168.1.12:8060',
-  }),
+    new Roku('roku', {
+        url: 'http://192.168.1.12:8060',
+    }),
 );
 
 async function discover() {
-  const rokus = await Roku.discoverAll();
-  rokus.forEach(roku => {
-    console.log(roku);
-  });
+    const rokus = await Roku.discoverAll();
+    rokus.forEach((roku) => {
+        console.log(roku);
+    });
 }
 
 async function start() {
-  await unisonht.start();
-  console.log(`Listening http://localhost:${port}`);
+    await unisonht.start();
+    console.log(`Listening http://localhost:${port}`);
 }
 
 start();
